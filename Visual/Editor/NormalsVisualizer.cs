76374c96e5b17718a1ad0622712852d028af6702
hww <valery.hww@gmail.com>
2022-09-20 13:30:01 +0300

    fix(config)

---
+++ Visual/Editor/NormalsVisualizer.cs
@@ -0,0 +1,52 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(MeshFilter))]
+[CanEditMultipleObjects]
+public class NormalsVisualizer : Editor
+{
+    public static float noramlLength = 0.5f;
+    public static bool displayNormals;
+
+    private Mesh mesh;
+
+    void OnEnable()
+    {
+        MeshFilter mf = target as MeshFilter;
+        if (mf != null)
+            mesh = mf.sharedMesh;
+    }
+
+    [MenuItem("/XiTools/Normals/Show")]
+    private static void DisplayNormals()
+    {
+        displayNormals = true;
+    }
+
+    [MenuItem("/XiTools/Normals/Hide")]
+    private static void HideNormals()
+    {
+        displayNormals = false;
+    }
+
+
+    void OnSceneGUI()
+    {
+        if (!displayNormals)
+            return;
+        if (mesh == null)
+            return;
+        if (mesh.vertices == null || mesh.vertices.Length == 0)
+            return;
+        if (mesh.normals == null || mesh.normals.Length == 0)
+            return;
+        for (int i = 0; i < mesh.vertexCount; i++)
+        {
+            Handles.matrix = (target as MeshFilter).transform.localToWorldMatrix;
+            Handles.color = Color.yellow;
+            Handles.DrawLine(
+                mesh.vertices[i],
+                mesh.vertices[i] + mesh.normals[i] * noramlLength);
+        }
+    }
+}
